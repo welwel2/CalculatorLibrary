aab7dfff252813ab239ddc8f8ba26d77bf3ffef1
Walid Seif <welwel2@yahoo.com>
2023-05-11 12:03:46 -0600

    fix subtraction test

--- test_calculator.py
+++ test_calculator.py
@@ -5,7 +5,7 @@ class TestCalculator:
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
-    def tes_subtraction(self):
+    def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
