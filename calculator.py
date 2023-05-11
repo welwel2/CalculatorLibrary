54615dfe378634b32643ed162e559ee3d5e8540f
Walid Seif <welwel2@yahoo.com>
2023-05-11 12:28:57 -0600

    add division

--- calculator.py
+++ calculator.py
@@ -13,3 +13,7 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def division(first_term, second_term):
+    return first_term / second_term
--- test_calculator.py
+++ test_calculator.py
@@ -10,3 +10,6 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+        assert 4 == calculator.division(8, 2)
